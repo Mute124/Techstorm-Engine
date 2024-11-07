3ffd393fad38200393f57969ff2b55b15e7719f8
Mute <rsteinke1111@gmail.com>
2024-11-07 14:44:57 +0100

    Started work on Config registry

--- src/engine/conf/Config.h
+++ src/engine/conf/Config.h
@@ -1,5 +1,16 @@
 #pragma once
-#include <libconfig.h>
+#include <unordered_map>
+#include <string>
+#include <libconfig.h++>
 
 namespace Techstorm {
+	class ConfigFileRegistry {
+	public:
+		ConfigFileRegistry(const std::string& searchPath);
+		ConfigFileRegistry();
+		~ConfigFileRegistry();
+
+	protected:
+		std::unordered_map<std::string, libconfig::Config> mRegistry;
+	};
 }
