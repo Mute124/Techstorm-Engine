ebcbc63dc217296d240c2f55bed6733246c3e25d
Mute <rsteinke1111@gmail.com>
2024-11-15 02:33:26 +0100

    f

--- Build.py
+++ Build.py
@@ -3,7 +3,8 @@ import subprocess
 if __name__ == "__main__":
     # If windows, then call build-windows.bat. Otherwise, call build-linux.sh
     if (os.name == "nt"):
-        subprocess.call('build-windows.bat')
+        subprocess.call(['build-windows.bat'])
+        
     else:
-        subprocess.call('chmod +x build-linux.sh')
+        subprocess.call(['chmod', '+x', 'build-linux.sh'])
         subprocess.call('./build-linux.sh') 
